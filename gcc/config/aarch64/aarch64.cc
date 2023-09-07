105157692f0466d9f646ad471e6f0666e0d42e3c
Francois-Xavier Coudert <fxcoudert@gcc.gnu.org>
2023-09-07 07:33:42 +0200

    aarch64, Darwin: Support IEEE754 Quad Precision as __float128.
    aarch64-apple-darwin has a C long double type which is the same as the
    double. In order to support a higher precision floating point type,
    similarly to x86_64-apple-darwin, we define define __float128 (and
    _Float128).
    
    Co-authored-by: Iain Sandoe <iain@sandoe.co.uk>
    Signed-off-by: Iain Sandoe <iain@sandoe.co.uk>
    
    gcc/ChangeLog:
    
    	* config/aarch64/aarch64-builtins.cc (AARCH64_BUILTIN_HUGE_VALQ,
    	AARCH64_BUILTIN_INFQ): New built-ins.
    	(aarch64_init_float128_types): New function...
    	(aarch64_general_init_builtins): ... called here.
    	(aarch64_general_fold_builtin): Initialize the new built-ins.
    	* config/aarch64/aarch64.cc (aarch64_gimplify_va_arg_expr):
    	Handle __float128 on darwin.
    	(aarch64_mangle_type): Mangle __float128.
    	(aarch64_c_mode_for_suffix): Assign suffix q to __float128.
    	(aarch64_scalar_mode_supported_p): Ensure TFmode is allowed.
    	* config/aarch64/aarch64.h (aarch64_float128_ptr_type_node): New
    	node.
    	* ginclude/stddef.h (__max_align_f128): Ensure it has correct
    	alignment.
    
    gcc/testsuite/ChangeLog:
    
    	* g++.target/aarch64/float128-darwin-1.C: New test.
    	* gcc.target/aarch64/darwin/float128-01.c: New test.
    
    libgcc/ChangeLog:
    
    	* config.host: Enable soft-float.
    	* config/aarch64/sfp-machine.h: Provide definitions for
    	aarch64-apple-darwin.
    	* config/aarch64/t-darwin: Fix comment.
    
    (cherry picked from commit 509899a70c0465900632a6237a25cd7d19f1118e)

--- gcc/config/aarch64/aarch64.cc
+++ gcc/config/aarch64/aarch64.cc
@@ -20547,8 +20547,18 @@ aarch64_gimplify_va_arg_expr (tree valist, tree type, gimple_seq *pre_p,
 	  field_ptr_t = double_ptr_type_node;
 	  break;
 	case E_TFmode:
-	  field_t = long_double_type_node;
-	  field_ptr_t = long_double_ptr_type_node;
+	  if (TARGET_MACHO)
+	    {
+	      /* Darwin has __float128, and long double is the same as
+		 double.  */
+	      field_t = float128_type_node;
+	      field_ptr_t = aarch64_float128_ptr_type_node;
+	    }
+	  else
+	    {
+	      field_t = long_double_type_node;
+	      field_ptr_t = long_double_ptr_type_node;
+	    }
 	  break;
 	case E_HFmode:
 	  field_t = aarch64_fp16_type_node;
@@ -21468,6 +21478,12 @@ aarch64_mangle_type (const_tree type)
       else
 	return "Dh";
     }
+
+  /* __float128 is mangled as "g" on darwin.  _Float128 is not mangled here,
+     but handled in common code (as "DF128_").  */
+  if (TARGET_MACHO && TYPE_MODE (type) == TFmode
+      && TYPE_MAIN_VARIANT (type) == float128_type_node)
+    return "g";
 
   /* Mangle AArch64-specific internal types.  TYPE_NAME is non-NULL_TREE for
      builtin types.  */
@@ -23869,7 +23885,7 @@ aarch64_init_libfuncs (void)
 static machine_mode
 aarch64_c_mode_for_suffix (char suffix)
 {
-  if (suffix == 'q' && !TARGET_MACHO)
+  if (suffix == 'q')
     return TFmode;
 
   return VOIDmode;
@@ -27208,12 +27224,14 @@ aarch64_libgcc_floating_mode_supported_p (scalar_float_mode mode)
 }
 
 /* Implement TARGET_SCALAR_MODE_SUPPORTED_P - return TRUE
-   if MODE is HFmode, and punt to the generic implementation otherwise.  */
+   if MODE is [BH]Fmode, or TFmode on Mach-O, and punt to the generic
+   implementation otherwise.  */
 
 static bool
 aarch64_scalar_mode_supported_p (scalar_mode mode)
 {
-  return (mode == HFmode
+
+  return ((mode == HFmode || mode == BFmode || (mode == TFmode && TARGET_MACHO))
 	  ? true
 	  : default_scalar_mode_supported_p (mode));
 }
